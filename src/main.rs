63998e3222a3b3483b7c761d3894b61e989384fc
Alex K <smileyman5@twcny.rr.com>
2023-03-08 22:29:27 -1000

    My version of the first word problem in ch4-3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Need to input a valid string");
+    
+    let result = first_word(&input);
+    println!("The first word is: {}", result);
+}
+
+fn first_word(s: &String) -> &str {
+    let mut words = s.split(' ');
+    match words.next() {
+        Some(word) => word,
+        None => &s,
+    }
 }
