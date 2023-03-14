ea190c29fe78689640f7b74ec1252da6b7075d09
Alex K <smileyman5@twcny.rr.com>
2023-03-13 23:25:17 -1000

    Testing structs in ch05-01

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::io::{self};
-
-fn main() {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Need to input a valid string");
-    
-    let result = first_word(&input);
-    println!("The first word is: {}", result);
+#[derive(Debug, Clone)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: i32,
+    active: bool,
 }
 
-fn first_word(s: &String) -> &str {
-    let mut words = s.split(' ');
-    match words.next() {
-        Some(word) => word,
-        None => &s,
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("SampleUser"),
+        email: String::from("sample@email.com"),
+        sign_in_count: 5,
+    };
+
+    let mut user2 = new_user_with(user1.clone(), String::from("sample2@email.com"));
+    user2.active = false;
+    
+    println!("The user: {:#?} {:#?}", user1, user2);
+}
+
+fn new_user_with(prime_user: User, email: String) -> User {
+    User {
+        email,
+        ..prime_user
     }
 }
