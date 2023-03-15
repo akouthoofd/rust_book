d2a2760f8b2f8c08770a3deda508f1348c704776
Alex K <smileyman5@twcny.rr.com>
2023-03-14 19:55:21 -1000

    Trying structs with impl containing associated functions + methods

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,34 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.width > other_rect.width && self.height > other_rect.height
     }
 }
 
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle::square(50);
 
-    println!(
-        "The area of the rectangle is {} square pixels",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    dbg!(rect1.area(), rect2.area(), rect3.area());
 }
