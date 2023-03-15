930a8e32761d5b4f292dfabaddf03785575f2bbc
Alex K <smileyman5@twcny.rr.com>
2023-03-14 19:19:26 -1000

    Testing structs with impl and dbg macro (ch05-03)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
 #[derive(Debug, Clone)]
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: i32,
-    active: bool,
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("SampleUser"),
-        email: String::from("sample@email.com"),
-        sign_in_count: 5,
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
 
-    let mut user2 = new_user_with(user1.clone(), String::from("sample2@email.com"));
-    user2.active = false;
-    
-    println!("The user: {:#?} {:#?}", user1, user2);
-}
-
-fn new_user_with(prime_user: User, email: String) -> User {
-    User {
-        email,
-        ..prime_user
-    }
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
 }
