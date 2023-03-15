9f1a7fe4ef69124d9773ad6bfe7c6de31851ba48
Alex K <smileyman5@twcny.rr.com>
2023-03-14 20:34:47 -1000

    Testing match vs if let syntax ch06-03

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,36 @@
 #[derive(Debug, Clone)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum Shapes {
+    Rectangle(u32, u32),
+    Circle {
+        radius: f32
+    }
 }
 
-impl Rectangle {
+impl Shapes {
     fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
+        Self::Rectangle(size, size)
+    }
+
+    fn area(&self) -> f32 {
+        match self {
+            Shapes::Rectangle(width, height) => (width * height) as f32,
+            Shapes::Circle { radius } => 2f32 * radius * 3.14
         }
     }
 
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other_rect: &Rectangle) -> bool {
-        self.width > other_rect.width && self.height > other_rect.height
+    fn sides(&self) -> Option<u32> {
+        if let Shapes::Rectangle(_, _) = self {
+            Some(4)
+        } else {
+            None
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle::square(50);
+    let rect = Shapes::Rectangle(30, 50);
+    let circle = Shapes::Circle { radius: 13.0 };
+    let square = Shapes::square(50);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    dbg!(rect1.area(), rect2.area(), rect3.area());
+    dbg!(rect.area(), rect.sides(), circle.area(), circle.sides(), square.area(), square.sides());
 }
